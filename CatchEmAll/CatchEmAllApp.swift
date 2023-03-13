3a8bfb7c9aa9b444ab024e745380f15dcf7eefc9
Christopher Kennedy <kennednu@bc.edu>
2023-03-12 22:23:56 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Christopher Kennedy on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
